30719f70e5bce5d6dc24fe51118764d44392a0b9
Samantha McCall <samanthaadams.biz@gmail.com>
2018-03-31 23:08:37 -0500

    adding missing semi-colons

--- liri.js
+++ liri.js
@@ -49,88 +49,91 @@ function myTweets() {
             for (var i = 0; i < tweets.length; i++) {
 
                 console.log(tweets[i].text);
-            } 
+            };
         } else {
             console.log("error: " + err);
             return;
-        }
+        };
     });
 };
 
-    //command 2 spotify this song
-    // need artist, song name, preview, album
-    function spotifyThisSong(trackName) {
-        var trackName = process.argv[3];
-        if (!trackName) {
-            trackName = "The Sign";
-        }
-        songRequest = trackName;
-        spotify.search({
-            type: "track",
-            query: songRequest
-        },
-            function (err, data) {
-                if (!err) {
-                    var trackInfo = data.tracks.items;
-                    for (var i = 0; i < 5; i++) {
-                        if (trackInfo[i] != undefined) {
-                            var spotifyResults =
-                                "Artist: " + trackInfo[i].artists[0].name + "\n" +
-                                "Song: " + trackInfo[i].name + "\n" +
-                                "Preview URL: " + trackInfo[i].preview_url + "\n" +
-                                "Album: " + trackInfo[i].album.name + "\n"
-
-                            console.log(spotifyResults);
-                            console.log(' ');
-                        }
-                    }
-                } else {
-                    console.log("error: " + err);
-                    return;
-                }
-            });
+//command 2 spotify this song
+// need artist, song name, preview, album
+function spotifyThisSong(trackName) {
+    var trackName = process.argv[3];
+    if (!trackName) {
+        trackName = "The Sign";
     };
-    //command 3 movie this
-    // run a request to the OMDB API with the movie specified
-    function movieThis() {
+    songRequest = trackName;
+    spotify.search({
+        type: "track",
+        query: songRequest
+    },
+        function (err, data) {
+            if (!err) {
+                var trackInfo = data.tracks.items;
+                for (var i = 0; i < 5; i++) {
+                    if (trackInfo[i] != undefined) {
+                        var spotifyResults =
+                            "Artist: " + trackInfo[i].artists[0].name + "\n" +
+                            "Song: " + trackInfo[i].name + "\n" +
+                            "Preview URL: " + trackInfo[i].preview_url + "\n" +
+                            "Album: " + trackInfo[i].album.name + "\n"
 
-        //using movieName from var list at top
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-        request(queryUrl, function (error, response, body) {
-
-            // If the request is successful
-            if (!error && response.statusCode === 200) {
-
-                //pull requested data in readable format
-                var myMovieData = JSON.parse(body);
-                var queryUrlResults =
-                    "Title: " + myMovieData.Title + "\n" +
-                    "Year: " + myMovieData.Year + "\n" +
-                    "IMDB Rating: " + myMovieData.Ratings[0].Value + "\n" +
-                    "Rotten Tomatoes Rating: " + myMovieData.Ratings[1].Value + "\n" +
-                    "Origin Country: " + myMovieData.Country + "\n" +
-                    "Language: " + myMovieData.Language + "\n" +
-                    "Plot: " + myMovieData.Plot + "\n" +
-                    "Actors: " + myMovieData.Actors + "\n"
-
-                console.log(queryUrlResults);
+                        console.log(spotifyResults);
+                        console.log(' ');
+                    };
+                };
+            } else {
+                console.log("error: " + err);
+                return;
             };
         });
-    };
+};
+//command 3 movie this
+// run a request to the OMDB API with the movie specified
+function movieThis() {
 
-    //command 4 do-what-it-says
-    // This block of code creates a file called "random.txt"
-    // It also adds the spotify command
-    function doWhatItSays() {
-        fs.writeFile("random.txt", 'spotify-this-song,"The Sign"', function (err) {
+    //using movieName from var list at top
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 
-            // If the code experiences any errors it will log the error to the console.
-            if (err) {
-                return console.log(err);
-            }
+    request(queryUrl, function (error, response, body) {
 
-            // Otherwise, it will print:
-            console.log("Rock out to 'The Sign'!");
-        });
-    };
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            //pull requested data in readable format
+            var myMovieData = JSON.parse(body);
+            var queryUrlResults =
+                "Title: " + myMovieData.Title + "\n" +
+                "Year: " + myMovieData.Year + "\n" +
+                "IMDB Rating: " + myMovieData.Ratings[0].Value + "\n" +
+                "Rotten Tomatoes Rating: " + myMovieData.Ratings[1].Value + "\n" +
+                "Origin Country: " + myMovieData.Country + "\n" +
+                "Language: " + myMovieData.Language + "\n" +
+                "Plot: " + myMovieData.Plot + "\n" +
+                "Actors: " + myMovieData.Actors + "\n"
+
+            console.log(queryUrlResults);
+        } else {
+            console.log("error: " + err);
+            return;
+        };
+    });
+};
+
+//command 4 do-what-it-says
+// This block of code creates a file called "random.txt"
+// It also adds the spotify command
+function doWhatItSays() {
+    fs.writeFile("random.txt", 'spotify-this-song,"The Sign"', function (err) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        };
+
+        // Otherwise, it will print:
+        console.log("Rock out to 'The Sign'!");
+    });
+};
